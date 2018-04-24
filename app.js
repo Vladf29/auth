2cb958f804092e979a3bdcd8e9974d13e1e63f63
Vladf29 <boxforemailand@gmail.com>
2018-04-24 22:15:07 +0300

    db

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/auth');
 
 const app = express();
 
--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,32 @@
 'use strict'
 
+const User = require('../modules/user');
+
 module.exports = {
     signUp: async (req, res, next) => {
-        console.log(req.value.body);
-        console.log('User con sign up');
+        const {
+            email,
+            password
+        } = req.value.body;
+
+        const foundUser = await User.findOne({
+            email
+        });
+        if (foundUser) {
+            return res.status(403).json({
+                error: 'Email is already in use'
+            });
+        }
+
+        const newUser = new User({
+            email,
+            password
+        });
+        await newUser.save();
+        
+        res.json({
+            user: 'create'
+        });
     },
     signIn: async (req, res, next) => {
         console.log('User con sign in');
